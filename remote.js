17a74058cf6364aa5fcc409f2d9420a053ce9f23
Lazaro Edreniel Millares Bermudez <lazaro.millares@tutanota.com>
2020-08-09 23:55:06 -0400

    first commit

--- remote.js
+++ remote.js
@@ -1,1 +1,9 @@
-alert('123');
+document.addEventListener("deviceready", function(){
+  var app = $('#app');
+  var number1 = $('<input type="number" name="number1"/>');
+  var number2 = $('<input type="number" name="number2"/>');
+  var button = $('<button>SUM</button>');
+  app.append(number1);
+  app.append(number2);
+  app.append(button);
+});
